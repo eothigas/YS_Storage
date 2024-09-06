0d849e93a0f65c077b9c44ce7f5f3cd9016680c2
Thiago <thi16thiago@gmail.com>
2024-09-06 17:18:31 -0300

    ajustes funcionalidades

--- process_form.php
+++ process_form.php
@@ -34,9 +34,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Executa a consulta
         $stmt->execute();
 
-        // Redireciona para a página de confirmação
-        header("Location: orcamentopendente.html"); // Certifique-se de criar esta página de confirmação
-        exit();
+        // Configurações do e-mail
+        $to = $email;
+        $subject = "Confirmação de Orçamento";
+        $message = "Olá $nome,\n\nAgradecemos pelo envio das suas informações! Gostaríamos de informar que recebemos e salvamos os seus dados com sucesso.\n\n ";
+        $headers .= "Reply-To: contato@suempresa.com\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        // Envia o e-mail
+        if (mail($to, $subject, $message, $headers)) {
+            // Redireciona para a página de confirmação após o e-mail ser enviado com sucesso
+            header("Location: orcamentopendente.html");
+            exit();
+        } else {
+            // Se o e-mail não foi enviado, você pode redirecionar para uma página de erro ou mostrar uma mensagem de erro
+            echo "Email não enviado";
+            exit();
+        }
     } catch (PDOException $e) {
         // Redireciona para a página de erro se necessário
         // header("Location: erro.html"); // Opcional: Página de erro genérica
