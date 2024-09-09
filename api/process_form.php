8b0a7b075363b0116e610d2e122ad0ff12553d11
Thiago <thi16thiago@gmail.com>
2024-09-08 21:12:44 -0300

    testando Vercel

--- api/process_form.php
+++ api/process_form.php
@@ -4,8 +4,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Configurações do banco de dados
     $host = 'localhost';
     $dbname = 'tqvguepm_orcamento';
-    $user = 'tqvguepm_orcamento';
-    $password = 'yourstorage';
+    $user = 'tqvguepm_Thiago';
+    $password = 'editorys';
 
     $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
 
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mensagem = $_POST['mensagem'];
 
         // Prepara a consulta SQL
-        $sql = "INSERT INTO cliente (nome, email, telefone, mensagem) VALUES (:nome, :email, :telefone, :mensagem)";
+        $sql = "INSERT INTO envio (nome, email, telefone, mensagem) VALUES (:nome, :email, :telefone, :mensagem)";
         $stmt = $pdo->prepare($sql);
 
         // Vincula os parâmetros
@@ -36,9 +36,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Configurações do e-mail
         $to = $email;
-        $subject = "Confirmação de Orçamento";
-        $message = "Olá $nome,\n\nAgradecemos pelo envio das suas informações! Gostaríamos de informar que recebemos e salvamos os seus dados com sucesso.\n\n ";
-        $headers .= "Reply-To: contato@suempresa.com\r\n";
+        $subject = "Recebimento de Formulário - Your Storage";
+        $message = "Olá $nome,\n\n Agradecemos pelo envio das suas informações! Gostaríamos de informar que recebemos e salvamos os dados em nosso banco de dados com sucesso.\n\n
+        Um de nossos agentes comerciais entrará em contato com você dentro de 1 a 2 dias úteis para discutir mais detalhes e esclarecer quaisquer dúvidas que você possa ter. O contato será feito através do e-mail e telefone fornecidos no formulário.n\n\n
+        Agradecemos por escolher nossa empresa e pelo tempo dedicado para preencher o formulário. Valorizamos cada dado recebido e faremos uma análise cuidadosa para garantir que possamos oferecer o melhor atendimento possível.\n\n
+        Se precisar de mais informações ou tiver alguma dúvida imediata, não hesite em nos contatar.\r\n
+        Atenciosamente,\r\n
+        Thiago Freitas // Your Storage
+        ";
+
+
         $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
         // Envia o e-mail
@@ -53,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     } catch (PDOException $e) {
         // Redireciona para a página de erro se necessário
-        // header("Location: erro.html"); // Opcional: Página de erro genérica
+        header("Location: index.html"); // Opcional: Página de erro genérica
         exit();
     }
 }
