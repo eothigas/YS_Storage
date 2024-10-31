3c3d7fd7dcb7db8e40acd7881569cdbd414c4581
Thiago <thi16thiago@gmail.com>
2024-10-30 23:57:36 -0300

    Repaginando sistema de Funcionários

--- funcionario/src/js/dashboard.js
+++ funcionario/src/js/dashboard.js
@@ -1,3 +1,24 @@
+document.getElementById('arrow').onclick = function(event) {
+    const modalUser = document.querySelector('.modal-user');
+    modalUser.style.display = modalUser.style.display === "flex" ? "none" : "flex";
+    document.getElementById('arrow').classList.toggle('rotate');
+    
+    // Impede que o evento clique no "arrow" feche o modal imediatamente
+    event.stopPropagation(); 
+};
+
+// Fecha o modal se clicar fora dele
+document.addEventListener('click', function(event) {
+    const modalUser = document.querySelector('.modal-user');
+    const arrow = document.getElementById('arrow');
+    
+    if (modalUser.style.display === "flex" && !modalUser.contains(event.target) && event.target !== arrow) {
+        modalUser.style.display = "none";
+        arrow.classList.remove('rotate');
+    }
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
     loadDashboardData(); //  Carregar dados na home
     fetchClientes(); // Buscar 5 usuários na tabela usuários e exibir 
