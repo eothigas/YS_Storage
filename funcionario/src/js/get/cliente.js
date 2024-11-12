d7fdae931739bde5b58955c6022b13449e83858e
Thiago <thi16thiago@gmail.com>
2024-11-12 01:44:39 -0300

    Funções JS próprias do HTML

--- funcionario/src/js/get/cliente.js
+++ funcionario/src/js/get/cliente.js
@@ -1,26 +1,3 @@
-// Carregar arquivo de imagem e exibir
-
-document.getElementById('upload-file').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const fileInfoText = document.getElementById('file-info-text');
-    const imagePreview = document.getElementById('image-preview');
-
-    if (file) {
-        const reader = new FileReader();
-        
-        reader.onload = function(e) {
-            imagePreview.src = e.target.result;
-            imagePreview.style.display = 'block'; // Mostra a prévia da imagem
-        };
-
-        reader.readAsDataURL(file);
-        fileInfoText.textContent = file.name; // Atualiza o texto para mostrar o nome do arquivo
-    } else {
-        fileInfoText.textContent = 'Nenhuma imagem selecionada';
-        imagePreview.style.display = 'none'; // Esconde a prévia se não houver imagem
-    }
-});
-
 //  Funções ver senha
 
 document.getElementById('view').addEventListener('click', function() {
@@ -82,30 +59,3 @@ document.getElementById('passwordInput').addEventListener('input', function() {
         strengthBar.appendChild(strength);
     }
 });
-
-// Mostrar Preview de Imagem, e Prencher a prévia 
-
-function previewImage(event) {
-    const file = event.target.files[0];
-    const imagePreview = document.getElementById('image-preview');
-    const avatarPreview = document.getElementById('avatar-preview');
-    const iconImage = document.getElementById('icon-image');
-
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            imagePreview.src = e.target.result;
-            imagePreview.style.display = 'block';
-            avatarPreview.src = e.target.result;
-            avatarPreview.style.display = 'block';
-            iconImage.style.display = 'none';
-        };
-        reader.readAsDataURL(file);
-        document.getElementById('file-info-text').innerText = file.name;
-    } else {
-        imagePreview.style.display = 'none';
-        avatarPreview.style.display = 'none';
-        iconImage.style.display = 'block';
-        document.getElementById('file-info-text').innerText = 'Nenhuma imagem selecionada';
-    }
-}
